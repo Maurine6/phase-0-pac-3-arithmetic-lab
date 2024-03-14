a82e8d6c4ba0907fea4d63bb9efa52b32d847ab2
omondimaurine <omondimaurine713@gmail.com>
2024-03-14 15:18:09 +0300

    writing basic arithmetics in javascript

---
+++ index.js
@@ -0,0 +1,63 @@
+function add(a,b){
+    return a +b;
+
+
+}
+console.log(add(590,2))
+console.log(add(660,16))
+console.log(add(1000,48))
+
+function subtract(a,b){
+    return a-b;
+
+}
+
+function multiply(a,b){
+    return a*b
+
+}
+
+function divide(a,b){
+    return a/b
+
+}
+
+function increment(n){
+    return ++n
+
+}
+console.log(increment(841))
+
+function decrement(n){
+    return --n
+}
+console.log(decrement(11))
+
+function makeInt(n){
+    return n
+
+}
+
+function Number(n){
+    return NAN
+}
+function makeInt(n){
+    return parseInt(n,10)
+
+}
+
+function preserveDecimal(n="2.222"){
+    parseFloat(2.222);
+    return 2.222
+
+
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+    
+
+}
+
+
+
+
